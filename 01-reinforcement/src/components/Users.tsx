457d2b94c0852eda5d8ae6f41447ccd49d54de36
CesarArellano <raywayday@gmail.com>
2021-11-12 00:35:16 -0600

    Adding simple pagination

--- 01-reinforcement/src/components/Users.tsx
+++ 01-reinforcement/src/components/Users.tsx
@@ -1,16 +1,57 @@
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 import { reqResApi } from '../api/reqRes';
-import { ReqResList } from "../interfaces/reqRes";
+import { ReqResList, User } from "../interfaces/reqRes";
 
 export const Users = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  const pageRef = useRef(1);
 
   useEffect( () => {
     // Call to API
-    reqResApi.get<ReqResList>('/users').then((resp) => {
-      console.log(resp.data.data[0].email);
-      
-    }).catch( console.log )
+    loadUsers();
   }, [])
+
+  const loadUsers = async () => {
+    try {
+      const resp = await reqResApi.get<ReqResList>('/users', {
+        params: {
+          page: pageRef.current
+        }
+      });
+      console.log(pageRef.current);
+      
+      
+      if( resp.data.data.length > 0 ) {
+        setUsers( resp.data.data );
+        pageRef.current++;
+      } else {
+        alert('No hay más registros');
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const renderItem = ({ id, avatar, first_name, last_name, email }: User) => {
+    return (
+      <tr key={ id.toString() }>
+        <td>
+          <img 
+            src={ avatar }
+            alt={ first_name }
+            style={{
+              width: 120,
+              borderRadius: 100
+            }}
+          />
+        </td>
+        <td>{ first_name } { last_name}</td>
+        <td>{ email }</td>
+      </tr>
+    );
+  }
+
 
   return (
     <>
@@ -24,9 +65,17 @@ export const Users = () => {
           </tr>
         </thead>
         <tbody>
-
+          {
+            users.map( renderItem )
+          }
         </tbody>
       </table>
+      <button
+        className="btn btn-primary"
+        onClick={ loadUsers }
+      >
+        Siguientes
+      </button>
     </>
   )
 }
