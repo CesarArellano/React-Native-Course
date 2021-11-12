f22e8381522726b0747569d72a9c7d8a766a7bac
CesarArellano <raywayday@gmail.com>
2021-11-12 00:05:51 -0600

    Adding and setting axios

---
+++ 01-reinforcement/src/components/Users.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+import { reqResApi } from '../api/reqRes';
+
+export const Users = () => {
+
+  useEffect( () => {
+    // Call to API
+    reqResApi.get('/users').then((resp) => {
+      console.log(resp.data.data);
+      
+    }).catch( console.log )
+  }, [])
+
+  return (
+    <>
+      <h3>Users:</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </>
+  )
+}
