1f77fe24718eae52d564b38b9d6cc9f0b5b55dbe
CesarArellano <raywayday@gmail.com>
2021-11-21 22:29:59 -0600

    Practicing with BoxObjectModel

--- 02-MiPrimerApp/src/screens/BoxObjectModelScreen.tsx
+++ 02-MiPrimerApp/src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Box Object Model</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    justifyContent: 'center'
+  },
+  title: {
+    padding: 10,
+    fontSize: 24,
+    borderWidth: 5,
+    alignSelf: 'center'
+  }
+});
