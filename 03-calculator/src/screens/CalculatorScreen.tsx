cf815376cf5f1f521f7c0504c9a11d92cb7e54e8
CesarArellano <raywayday@gmail.com>
2021-11-24 23:24:29 -0600

    Adding styles and first button in CalculatorScreen

--- 03-calculator/src/screens/CalculatorScreen.tsx
+++ 03-calculator/src/screens/CalculatorScreen.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text style={ styles.text }>Calculator Screen</Text>
+    <View style={ styles.calculatorContainer }>
+      <Text style={ styles.smallMathResult }>150.00</Text>
+      <Text style={ styles.mathResult }>1500.00</Text>
+      <View>
+        <View style={ styles.button }>
+          <Text style={ styles.buttonText }>1</Text>
+        </View>
+      </View>
     </View>
   )
 }
--- 03-calculator/src/theme/appTheme.tsx
+++ 03-calculator/src/theme/appTheme.tsx
@@ -5,8 +5,34 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  text: {
+  calculatorContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+
+  },
+  mathResult: {
     color: 'white',
-    fontSize: 20
-  }
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  smallMathResult: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    backgroundColor: '#333333',
+    color: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '300'
+  },
 });
