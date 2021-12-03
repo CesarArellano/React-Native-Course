85499bc2e678c8a12d9c55753b1a408fbe4420ad
CesarArellano <raywayday@gmail.com>
2021-12-02 19:33:49 -0600

    Ignoring Reanimated2 LogBox

--- 04-navigationApp/App.tsx
+++ 04-navigationApp/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react'
 import 'react-native-gesture-handler';
 
-import React from 'react'
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Reanimated 2']);
 import { NavigationContainer } from '@react-navigation/native';
-// import { StackNavigator } from './src/navigator/StackNavigator';
 import { DrawerNavigator } from './src/navigator/DrawerNavigator';
+// import { StackNavigator } from './src/navigator/StackNavigator';
 
 const App = () => {
   return (
