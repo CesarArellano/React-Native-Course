6d2d3ba8f8e4f6a5da9147897943b7b46444ec3a
CesarArellano <raywayday@gmail.com>
2021-12-10 15:28:03 -0600

    Adding useSafeAreaInsets hook

--- 04-navigationApp/src/screens/SettingsScreen.tsx
+++ 04-navigationApp/src/screens/SettingsScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+// import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export const SettingsScreen = () => {
+  // const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text>Settings Screen</Text>
+    <View 
+      style={{ 
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // marginTop: insets.top * 0.5
+      }}
+    >
+      <Text style= { styles.menuText }>Settings Screen</Text>
     </View>
   )
 }
