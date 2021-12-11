94acece569663de77133f12e8301970057b9f268
CesarArellano <raywayday@gmail.com>
2021-12-11 11:17:18 -0600

    Preparing space to icons

--- 04-navigationApp/src/navigator/Tabs.tsx
+++ 04-navigationApp/src/navigator/Tabs.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Text } from 'react-native';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { colors } from '../theme/appTheme';
@@ -16,11 +17,31 @@ export const Tabs = () => {
         backgroundColor: 'white',
         elevation: 0,
       }}
-      screenOptions={{
+      screenOptions={ ({route}) => ({
         tabBarActiveTintColor: colors.primary,
-      }}
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName: string = '';
+          switch ( route.name ) {
+            case 'Tab1':
+              iconName = 'T1';
+              break;
+            case 'Tab2':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'T3';
+              break;
+          }
+
+          return <Text style={{ color, }}>{ iconName }</Text>
+        }
+      })}
     >
-      <Tab.Screen name="Tab1" options={{ title: 'Tab 1'}} component={ Tab1Screen } />
+      {/* <Tab.Screen name="Tab1" options={{ title: 'Tab 1', tabBarIcon: () => <Text>T1</Text>}} component={ Tab1Screen } /> */}
+      <Tab.Screen name="Tab1" options={{ title: 'Tab 1' }} component={ Tab1Screen } />
       <Tab.Screen name="Tab2" options={{ title: 'Tab 2'}} component={ Tab2Screen } />
       <Tab.Screen name="StackNavigator" options={{ title: 'Stack'}} component={ StackNavigator } />
     </Tab.Navigator>
