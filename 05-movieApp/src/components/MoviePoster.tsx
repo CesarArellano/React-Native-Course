f47f5abef085ba35a5f9c9243d6677f1eb885a2a
CesarArellano <raywayday@gmail.com>
2021-12-22 15:25:07 -0600

    Showing Movie Poster

---
+++ 05-movieApp/src/components/MoviePoster.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import { Movie } from '../interfaces/movie_interface';
+
+interface Props {
+  movie: Movie
+}
+
+export const MoviePoster = ({ movie }: Props ) => {
+  const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
+  console.log(uri);
+  
+  return (
+    <View style={{
+      width: 300,
+      height: 420,
+    }}>
+      <View style={ styles.imageContainer }>
+        <Image source={{ uri }} style={ styles.image }/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 15,
+  },
+  imageContainer: {
+    flex: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 5,
+    elevation: 6
+  }
+});
--- 05-movieApp/src/screens/HomeScreen.tsx
+++ 05-movieApp/src/screens/HomeScreen.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/core';
 import { ActivityIndicator, Button, Text, View} from 'react-native';
 import { useMovies } from '../hooks/useMovies';
+import { MoviePoster } from '../components/MoviePoster';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
   const { moviesInCinema, isLoading } = useMovies();
-  const navigation = useNavigation<any>();
+  const { top } = useSafeAreaInsets();
 
   console.log(moviesInCinema[0]?.title);
   
-  if( true ) {
+  if( isLoading ) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator color='red' size={ 100 }/>
@@ -18,12 +20,8 @@ export const HomeScreen = () => {
   }
 
   return (
-    <View>
-      <Text> Home Screen </Text>
-      <Button 
-        title="Detail Screen"
-        onPress={ () => navigation.navigate('DetailScreen') }
-      />
+    <View style={{ marginTop: top + 10 }}>
+      <MoviePoster movie={ moviesInCinema[2] }/>
     </View>
   );
 }
