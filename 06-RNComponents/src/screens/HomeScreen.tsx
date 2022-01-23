e0b24cc69e35c5d6722c947886a301db28afcda7
CesarArellano <raywayday@gmail.com>
2022-01-22 22:50:59 -0600

    Starting new section and Remebering Things of RN

---
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Icon name="add-outline" size={ 100 } color="#900" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
